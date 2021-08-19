cc2144c5a7550366ef65097d44b40c9370e3a840
Elijah Edun <edunelijah18@gmail.com>
2021-08-19 12:00:57 +0100

    simple name output program

--- name.js
+++ name.js
@@ -5,3 +5,5 @@ let middleName = "Oluwakayode";
 let lastName = "Edun";
 
 console.log("My names are " + firstName + " " + middleName + " " + lastName + ".");
+
+//output: My names are Elijah Oluwakayode Edun.
